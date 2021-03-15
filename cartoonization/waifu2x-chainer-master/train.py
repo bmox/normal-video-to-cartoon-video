15aaaf3e8b8e7cd1ddd0a89253d1f17591006c89
bmox <62886280+bmox@users.noreply.github.com>
2021-03-15 19:46:45 +0530

    Add files via upload

---
+++ cartoonization/waifu2x-chainer-master/train.py
@@ -0,0 +1,210 @@
+from __future__ import division
+from __future__ import print_function
+import argparse
+import os
+import shutil
+import time
+import warnings
+
+import chainer
+from chainer import optimizers
+import numpy as np
+import six
+
+from lib import iproc
+from lib import srcnn
+from lib import utils
+
+from lib.dataset_sampler import DatasetSampler
+from lib.loss import clipped_weighted_huber_loss
+
+
+def train_inner_epoch(model, weight, optimizer, data_queue, batch_size):
+    sum_loss = 0
+    xp = model.xp
+    train_x, train_y = data_queue.get()
+    perm = np.random.permutation(len(train_x))
+    for i in six.moves.range(0, len(train_x), batch_size):
+        local_perm = perm[i:i + batch_size]
+        batch_x = xp.array(train_x[local_perm], dtype=np.float32) / 255
+        batch_y = xp.array(train_y[local_perm], dtype=np.float32) / 255
+        model.cleargrads()
+        pred = model(batch_x)
+        # loss = F.mean_squared_error(pred, batch_y)
+        loss = clipped_weighted_huber_loss(pred, batch_y, weight)
+        loss.backward()
+        optimizer.update()
+        sum_loss += float(loss.data) * len(batch_x)
+    return sum_loss / len(train_x)
+
+
+def valid_inner_epoch(model, data_queue, batch_size):
+    sum_score = 0
+    xp = model.xp
+    valid_x, valid_y = data_queue.get()
+    perm = np.random.permutation(len(valid_x))
+    with chainer.no_backprop_mode(), chainer.using_config('train', False):
+        for i in six.moves.range(0, len(valid_x), batch_size):
+            local_perm = perm[i:i + batch_size]
+            batch_x = xp.array(valid_x[local_perm], dtype=np.float32) / 255
+            batch_y = xp.array(valid_y[local_perm], dtype=np.float32) / 255
+            pred = model(batch_x)
+            score = iproc.clipped_psnr(pred.data, batch_y)
+            sum_score += float(score) * len(batch_x)
+    return sum_score / len(valid_x)
+
+
+def main():
+    p = argparse.ArgumentParser(description='Chainer implementation of waifu2x')
+    p.add_argument('--gpu', '-g', type=int, default=-1)
+    p.add_argument('--seed', '-s', type=int, default=11)
+    p.add_argument('--dataset_dir', '-d', required=True)
+    p.add_argument('--validation_rate', type=float, default=0.05)
+    p.add_argument('--nr_rate', type=float, default=0.65)
+    p.add_argument('--chroma_subsampling_rate', type=float, default=0.5)
+    p.add_argument('--reduce_memory_usage', action='store_true')
+    p.add_argument('--out_size', type=int, default=64)
+    p.add_argument('--max_size', type=int, default=256)
+    p.add_argument('--active_cropping_rate', type=float, default=0.5)
+    p.add_argument('--active_cropping_tries', type=int, default=10)
+    p.add_argument('--random_half_rate', type=float, default=0.0)
+    p.add_argument('--random_color_noise_rate', type=float, default=0.0)
+    p.add_argument('--random_unsharp_mask_rate', type=float, default=0.0)
+    p.add_argument('--learning_rate', type=float, default=0.00025)
+    p.add_argument('--lr_min', type=float, default=0.00001)
+    p.add_argument('--lr_decay', type=float, default=0.9)
+    p.add_argument('--lr_decay_interval', type=int, default=5)
+    p.add_argument('--batch_size', '-b', type=int, default=16)
+    p.add_argument('--patches', '-p', type=int, default=64)
+    p.add_argument('--validation_crop_rate', type=float, default=0.5)
+    p.add_argument('--downsampling_filters', nargs='+', default=['box'])
+    p.add_argument('--resize_blur_min', type=float, default=0.95)
+    p.add_argument('--resize_blur_max', type=float, default=1.05)
+    p.add_argument('--epoch', '-e', type=int, default=50)
+    p.add_argument('--inner_epoch', type=int, default=4)
+    p.add_argument('--finetune', '-f', default=None)
+    p.add_argument('--model_name', default=None)
+    p.add_argument('--color', '-c', default='rgb',
+                   choices=['y', 'rgb'])
+    p.add_argument('--arch', '-a', default='VGG7',
+                   choices=['VGG7', '0', 'UpConv7', '1', 'ResNet10', '2', 'UpResNet10', '3'])
+    p.add_argument('--method', '-m', default='scale',
+                   choices=['noise', 'scale', 'noise_scale'],)
+    p.add_argument('--noise_level', '-n', type=int, default=1,
+                   choices=[0, 1, 2, 3])
+
+    args = p.parse_args()
+    if args.arch in srcnn.table:
+        args.arch = srcnn.table[args.arch]
+
+    utils.set_random_seed(args.seed, args.gpu)
+    if args.color == 'y':
+        ch = 1
+        weight = (1.0,)
+    elif args.color == 'rgb':
+        ch = 3
+        weight = (0.29891 * 3, 0.58661 * 3, 0.11448 * 3)
+    weight = np.array(weight, dtype=np.float32)
+    weight = weight[:, np.newaxis, np.newaxis]
+
+    print('* loading filelist...', end=' ')
+    filelist = utils.load_filelist(args.dataset_dir, shuffle=True)
+    valid_num = int(np.ceil(args.validation_rate * len(filelist)))
+    valid_list, train_list = filelist[:valid_num], filelist[valid_num:]
+    print('done')
+
+    print('* setup model...', end=' ')
+    if args.model_name is None:
+        if args.method == 'noise':
+            model_name = 'anime_style_noise{}'.format(args.noise_level)
+        elif args.method == 'scale':
+            model_name = 'anime_style_scale'
+        elif args.method == 'noise_scale':
+            model_name = 'anime_style_noise{}_scale'.format(args.noise_level)
+        model_path = '{}_{}.npz'.format(model_name, args.color)
+    else:
+        model_name = args.model_name.rstrip('.npz')
+        model_path = model_name + '.npz'
+    if not os.path.exists('epoch'):
+        os.makedirs('epoch')
+
+    model = srcnn.archs[args.arch](ch)
+    if model.offset % model.inner_scale != 0:
+        raise ValueError('offset %% inner_scale must be 0.')
+    elif model.inner_scale != 1 and model.inner_scale % 2 != 0:
+        raise ValueError('inner_scale must be 1 or an even number.')
+    if args.finetune is not None:
+        chainer.serializers.load_npz(args.finetune, model)
+
+    if args.gpu >= 0:
+        chainer.backends.cuda.check_cuda_available()
+        chainer.backends.cuda.get_device(args.gpu).use()
+        weight = chainer.backends.cuda.to_gpu(weight)
+        model.to_gpu()
+
+    optimizer = optimizers.Adam(alpha=args.learning_rate)
+    optimizer.setup(model)
+    print('done')
+
+    valid_config = utils.get_config(args, model, train=False)
+    train_config = utils.get_config(args, model, train=True)
+
+    print('* check forward path...', end=' ')
+    di = train_config.in_size
+    do = train_config.out_size
+    dx = model.xp.zeros((args.batch_size, ch, di, di), dtype=np.float32)
+    dy = model(dx)
+    if dy.shape[2:] != (do, do):
+        raise ValueError('Invlid output size\n'
+                         'Expect: {}\n'
+                         'Actual: ({}, {})'.format(dy.shape[2:], do, do))
+    print('done')
+
+    print('* starting processes of dataset sampler...', end=' ')
+    valid_queue = DatasetSampler(valid_list, valid_config)
+    train_queue = DatasetSampler(train_list, train_config)
+    print('done')
+
+    best_count = 0
+    best_score = 0
+    best_loss = np.inf
+    for epoch in range(0, args.epoch):
+        print('### epoch: {} ###'.format(epoch))
+        train_queue.reload_switch(init=(epoch < args.epoch - 1))
+        for inner_epoch in range(0, args.inner_epoch):
+            best_count += 1
+            print('  # inner epoch: {}'.format(inner_epoch))
+            start = time.time()
+            train_loss = train_inner_epoch(
+                model, weight, optimizer, train_queue, args.batch_size)
+            if args.reduce_memory_usage:
+                train_queue.wait()
+            if train_loss < best_loss:
+                best_loss = train_loss
+                print('    * best loss on training dataset: {:.6f}'.format(
+                    train_loss))
+            valid_score = valid_inner_epoch(
+                model, valid_queue, args.batch_size)
+            if valid_score > best_score:
+                best_count = 0
+                best_score = valid_score
+                print('    * best score on validation dataset: PSNR {:.6f} dB'
+                      .format(valid_score))
+                best_model = model.copy().to_cpu()
+                epoch_path = 'epoch/{}_epoch{}.npz'.format(model_name, epoch)
+                chainer.serializers.save_npz(model_path, best_model)
+                shutil.copy(model_path, epoch_path)
+            if best_count >= args.lr_decay_interval:
+                best_count = 0
+                optimizer.alpha *= args.lr_decay
+                if optimizer.alpha < args.lr_min:
+                    optimizer.alpha = args.lr_min
+                else:
+                    print('    * learning rate decay: {:.6f}'.format(
+                        optimizer.alpha))
+            print('    * elapsed time: {:.6f} sec'.format(time.time() - start))
+
+
+if __name__ == '__main__':
+    warnings.filterwarnings('ignore')
+    main()
---
+++ cartoonization/waifu2x-chainer-master/waifu2x.py
@@ -0,0 +1,239 @@
+from __future__ import division
+import argparse
+import os
+import time
+
+import chainer
+import numpy as np
+from PIL import Image
+import six
+
+from lib import iproc
+from lib import reconstruct
+from lib import srcnn
+from lib import utils
+
+
+def denoise_image(cfg, src, model):
+    dst, alpha = split_alpha(src, model)
+    six.print_('Level {} denoising...'.format(cfg.noise_level),
+               end=' ', flush=True)
+    if cfg.tta:
+        dst = reconstruct.image_tta(
+            dst, model, cfg.tta_level, cfg.block_size, cfg.batch_size)
+    else:
+        dst = reconstruct.image(dst, model, cfg.block_size, cfg.batch_size)
+    if model.inner_scale != 1:
+        dst = dst.resize((src.size[0], src.size[1]), Image.LANCZOS)
+    six.print_('OK')
+    if alpha is not None:
+        dst.putalpha(alpha)
+    return dst
+
+
+def upscale_image(cfg, src, scale_model, alpha_model=None):
+    dst, alpha = split_alpha(src, scale_model)
+    for i in range(int(np.ceil(np.log2(cfg.scale_ratio)))):
+        six.print_('2.0x scaling...', end=' ', flush=True)
+        model = scale_model if i == 0 or alpha_model is None else alpha_model
+        if model.inner_scale == 1:
+            dst = iproc.nn_scaling(dst, 2)  # Nearest neighbor 2x scaling
+            alpha = iproc.nn_scaling(alpha, 2)  # Nearest neighbor 2x scaling
+        if cfg.tta:
+            dst = reconstruct.image_tta(
+                dst, model, cfg.tta_level, cfg.block_size, cfg.batch_size)
+        else:
+            dst = reconstruct.image(dst, model, cfg.block_size, cfg.batch_size)
+        if alpha_model is None:
+            alpha = reconstruct.image(
+                alpha, scale_model, cfg.block_size, cfg.batch_size)
+        else:
+            alpha = reconstruct.image(
+                alpha, alpha_model, cfg.block_size, cfg.batch_size)
+        six.print_('OK')
+    dst_w = int(np.round(src.size[0] * cfg.scale_ratio))
+    dst_h = int(np.round(src.size[1] * cfg.scale_ratio))
+    if dst_w != dst.size[0] or dst_h != dst.size[1]:
+        six.print_('Resizing...', end=' ', flush=True)
+        dst = dst.resize((dst_w, dst_h), Image.LANCZOS)
+        six.print_('OK')
+    if alpha is not None:
+        if alpha.size[0] != dst_w or alpha.size[1] != dst_h:
+            alpha = alpha.resize((dst_w, dst_h), Image.LANCZOS)
+        dst.putalpha(alpha)
+    return dst
+
+
+def split_alpha(src, model):
+    alpha = None
+    if src.mode in ('L', 'RGB', 'P'):
+        if isinstance(src.info.get('transparency'), bytes):
+            src = src.convert('RGBA')
+    rgb = src.convert('RGB')
+    if src.mode in ('LA', 'RGBA'):
+        six.print_('Splitting alpha channel...', end=' ', flush=True)
+        alpha = src.split()[-1]
+        rgb = iproc.alpha_make_border(rgb, alpha, model)
+        six.print_('OK')
+    return rgb, alpha
+
+
+def load_models(cfg):
+    ch = 3 if cfg.color == 'rgb' else 1
+    if cfg.model_dir is None:
+        model_dir = 'models/{}'.format(cfg.arch.lower())
+    else:
+        model_dir = cfg.model_dir
+
+    models = {}
+    flag = False
+    if cfg.method == 'noise_scale':
+        model_name = 'anime_style_noise{}_scale_{}.npz'.format(
+            cfg.noise_level, cfg.color)
+        model_path = os.path.join(model_dir, model_name)
+        if os.path.exists(model_path):
+            models['noise_scale'] = srcnn.archs[cfg.arch](ch)
+            chainer.serializers.load_npz(model_path, models['noise_scale'])
+            alpha_model_name = 'anime_style_scale_{}.npz'.format(cfg.color)
+            alpha_model_path = os.path.join(model_dir, alpha_model_name)
+            models['alpha'] = srcnn.archs[cfg.arch](ch)
+            chainer.serializers.load_npz(alpha_model_path, models['alpha'])
+        else:
+            flag = True
+    if cfg.method == 'scale' or flag:
+        model_name = 'anime_style_scale_{}.npz'.format(cfg.color)
+        model_path = os.path.join(model_dir, model_name)
+        models['scale'] = srcnn.archs[cfg.arch](ch)
+        chainer.serializers.load_npz(model_path, models['scale'])
+    if cfg.method == 'noise' or flag:
+        model_name = 'anime_style_noise{}_{}.npz'.format(
+            cfg.noise_level, cfg.color)
+        model_path = os.path.join(model_dir, model_name)
+        if not os.path.exists(model_path):
+            model_name = 'anime_style_noise{}_scale_{}.npz'.format(
+                cfg.noise_level, cfg.color)
+            model_path = os.path.join(model_dir, model_name)
+        models['noise'] = srcnn.archs[cfg.arch](ch)
+        chainer.serializers.load_npz(model_path, models['noise'])
+
+    if cfg.gpu >= 0:
+        chainer.backends.cuda.check_cuda_available()
+        chainer.backends.cuda.get_device(cfg.gpu).use()
+        for _, model in models.items():
+            model.to_gpu()
+    return models
+
+
+def main():
+    p = argparse.ArgumentParser(description='Chainer implementation of waifu2x')
+    p.add_argument('--gpu', '-g', type=int, default=-1)
+    p.add_argument('--input', '-i', default='images/small.png')
+    p.add_argument('--output', '-o', default='./')
+    p.add_argument('--quality', '-q', type=int, default=None)
+    p.add_argument('--model_dir', '-d', default=None)
+    p.add_argument('--scale_ratio', '-s', type=float, default=2.0)
+    p.add_argument('--tta', '-t', action='store_true')
+    p.add_argument('--batch_size', '-b', type=int, default=16)
+    p.add_argument('--block_size', '-l', type=int, default=128)
+    p.add_argument('--extension', '-e', default='png',
+                   choices=['png', 'webp'])
+    p.add_argument('--arch', '-a', default='VGG7',
+                   choices=['VGG7', '0', 'UpConv7', '1', 'ResNet10', '2', 'UpResNet10', '3'])
+    p.add_argument('--method', '-m', default='scale',
+                   choices=['noise', 'scale', 'noise_scale'])
+    p.add_argument('--noise_level', '-n', type=int, default=1,
+                   choices=[0, 1, 2, 3])
+    p.add_argument('--color', '-c', default='rgb',
+                   choices=['y', 'rgb'])
+    p.add_argument('--tta_level', '-T', type=int, default=8,
+                   choices=[2, 4, 8])
+    g = p.add_mutually_exclusive_group()
+    g.add_argument('--width', '-W', type=int, default=0)
+    g.add_argument('--height', '-H', type=int, default=0)
+    g.add_argument('--shorter_side', '-S', type=int, default=0)
+    g.add_argument('--longer_side', '-L', type=int, default=0)
+
+    args = p.parse_args()
+    if args.arch in srcnn.table:
+        args.arch = srcnn.table[args.arch]
+
+    models = load_models(args)
+
+    input_exts = ['.png', '.jpg', '.jpeg', '.bmp', '.tif', '.tiff', '.webp']
+    output_exts = ['.png', '.webp']
+    outext = '.' + args.extension
+
+    outname = None
+    outdir = args.output
+    if os.path.isdir(args.input):
+        filelist = utils.load_filelist(args.input)
+    else:
+        tmpname, tmpext = os.path.splitext(os.path.basename(args.output))
+        if tmpext in output_exts:
+            outext = tmpext
+            outname = tmpname
+            outdir = os.path.dirname(args.output)
+            outdir = './' if outdir == '' else outdir
+        elif tmpext != '':
+            raise ValueError('Format {} is not supported'.format(tmpext))
+        filelist = [args.input]
+
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    for path in filelist:
+        tmpname, tmpext = os.path.splitext(os.path.basename(path))
+        if outname is None or len(filelist) > 1:
+            outname = tmpname
+        outpath = os.path.join(outdir, '{}{}'.format(outname, outext))
+        if tmpext.lower() in input_exts:
+            src = Image.open(path)
+            w, h = src.size[:2]
+            if args.width != 0:
+                args.scale_ratio = args.width / w
+            elif args.height != 0:
+                args.scale_ratio = args.height / h
+            elif args.shorter_side != 0:
+                if w < h:
+                    args.scale_ratio = args.shorter_side / w
+                else:
+                    args.scale_ratio = args.shorter_side / h
+            elif args.longer_side != 0:
+                if w > h:
+                    args.scale_ratio = args.longer_side / w
+                else:
+                    args.scale_ratio = args.longer_side / h
+
+            dst = src.copy()
+            start = time.time()
+            outname += '_(tta{})'.format(args.tta_level) if args.tta else '_'
+            if 'noise_scale' in models:
+                outname += '(noise{}_scale{:.1f}x)'.format(
+                    args.noise_level, args.scale_ratio)
+                dst = upscale_image(
+                    args, dst, models['noise_scale'], models['alpha'])
+            else:
+                if 'noise' in models:
+                    outname += '(noise{})'.format(args.noise_level)
+                    dst = denoise_image(args, dst, models['noise'])
+                if 'scale' in models:
+                    outname += '(scale{:.1f}x)'.format(args.scale_ratio)
+                    dst = upscale_image(args, dst, models['scale'])
+            print('Elapsed time: {:.6f} sec'.format(time.time() - start))
+
+            outname += '({}_{}){}'.format(args.arch, args.color, outext)
+            if os.path.exists(outpath):
+                outpath = os.path.join(outdir, outname)
+
+            lossless = args.quality is None
+            quality = 100 if lossless else args.quality
+            icc_profile = src.info.get('icc_profile')
+            icc_profile = "" if icc_profile is None else icc_profile
+            dst.convert(src.mode).save(
+                outpath, quality=quality, lossless=lossless,
+                icc_profile=icc_profile)
+            six.print_('Saved as \'{}\''.format(outpath))
+
+
+if __name__ == '__main__':
+    main()
